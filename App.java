89ca04fc2b5cc77d122c10071092c9695bec1c45
PratheekshaP3 <prxtheeksha@gmail.com>
2026-02-10 23:48:00 +0530

    Test

--- App.java
+++ App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Nightly and push build successful");
+ System.out.println("Second build successful");
     }
 }
